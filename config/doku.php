ef795a5f01e2f09c753dbff7554e4fc1e797bba4
leanderarya <aryaajisadda@gmail.com>
2026-07-05 17:44:24 +0700

    feat: add DOKU config and environment variables

---
+++ config/doku.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'client_id' => env('DOKU_CLIENT_ID'),
+    'api_key' => env('DOKU_API_KEY'),
+    'merchant_code' => env('DOKU_MERCHANT_CODE'),
+    'sandbox' => env('DOKU_IS_SANDBOX', true),
+    'base_url' => env('DOKU_IS_SANDBOX', true)
+        ? 'https://api-sandbox.doku.com'
+        : 'https://api.doku.com',
+];
