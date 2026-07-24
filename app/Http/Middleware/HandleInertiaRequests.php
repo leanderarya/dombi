afb34f573854196bfb33539f679d4cbdb9c509b0
leanderarya <aryaajisadda@gmail.com>
2026-07-24 19:28:39 +0700

    fix: share Inertia error flash

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -55,6 +55,7 @@ class HandleInertiaRequests extends Middleware
             ],
             'flash' => [
                 'success' => fn () => $request->session()->get('success'),
+                'error' => fn () => $request->session()->get('error'),
                 'outlet_provisioning' => fn () => $request->session()->get('outlet_provisioning'),
             ],
             'guestMode' => fn () => session('guest_mode', false),
