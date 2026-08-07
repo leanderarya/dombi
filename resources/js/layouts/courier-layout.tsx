ecba1e0ffa82bb6b860393602620a40832093006
leanderarya <aryaajisadda@gmail.com>
2026-08-07 17:52:19 +0700

    fix(courier): add 16px spacing between blue header and first card

--- resources/js/layouts/courier-layout.tsx
+++ resources/js/layouts/courier-layout.tsx
@@ -117,7 +117,7 @@ export default function CourierLayout({
                     )}
                 </header>
             )}
-            {children}
+            <div className={title ? '' : 'pt-4'}>{children}</div>
             <NotificationSheet
                 open={notificationOpen}
                 onClose={() => setNotificationOpen(false)}
