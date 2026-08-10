4f8ff4f58776394282f69fcbe4d2e80c112bb59d
leanderarya <aryaajisadda@gmail.com>
2026-08-10 10:09:58 +0700

    chore(pwa): replace Laravel favicon with Dombi logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         @endif
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.png" type="image/png">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         <link rel="manifest" href="/manifest.json">
 
