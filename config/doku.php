8654ea13a4789020caa0811f5341c90e386c89d5
leanderarya <aryaajisadda@gmail.com>
2026-08-24 15:39:19 +0700

    chore: configure cutover evidence

--- config/doku.php
+++ config/doku.php
@@ -37,4 +37,5 @@ return [
 
     'enabled_methods' => explode(',', env('DOKU_ENABLED_METHODS', 'qris,transfer,ewallet,credit_card')),
     'legacy_writes_enabled' => env('PAYMENTS_LEGACY_WRITES_ENABLED', false),
+    'legacy_writes_deployment_evidence' => env('PAYMENTS_LEGACY_WRITES_DEPLOYMENT_EVIDENCE'),
 ];
